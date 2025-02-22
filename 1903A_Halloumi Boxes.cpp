6c0d85096f279bbaca39e14d1a78fe3176d3a079
SaHaRsH45 <saharshkumar45@gmail.com>
2025-02-23 02:22:45 +0530

    done

---
+++ 1903A_Halloumi Boxes.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool sorted(vector<int>&arr){
+  int i=0;
+  while(i<arr.size()-1){
+    if(arr[i]>arr[i+1]){
+      return false;
+    }
+    i++;
+  }
+  return true;
+}
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int n,k;
+    cin>>n>>k;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+      cin>>arr[i];
+    }
+  if(k==1){
+    if(sorted(arr)){
+      cout<<"YES";
+    }
+    else{
+      cout<<"NO";
+    }
+  }
+  else{
+    cout<<"YES";
+  }
+  cout<<endl;
+  cout<<endl;
+  }
+  return 0;
+}
+
