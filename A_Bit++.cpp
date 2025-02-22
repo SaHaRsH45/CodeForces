56f61f339831f16762eebefaf83c24f0bc829417
SaHaRsH45 <saharshkumar45@gmail.com>
2025-02-23 03:01:56 +0530

    done

---
+++ A_Bit++.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int x=0;
+  while(n--){
+    string s;
+    cin>>s;
+    if(s=="++X" ||s=="X++"){
+      x=x+1;
+    }
+    else{
+      x=x-1;
+    }
+  }
+  cout<<x<<endl;
+  return 0;
+}
